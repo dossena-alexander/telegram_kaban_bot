e256bb9c303d3c63b351596d19d47399253b868f
Alexander <nidillin@gmail.com>
2022-06-19 18:49:55 +0300

    utils is package now

---
+++ bot/utils/__init__.py
@@ -0,0 +1,5 @@
+from utils.db import UserDB, BoarDB, JokeDB, MsgDB, PicDB
+from utils.menu import Menu
+from utils.upload import UploadPic
+from utils.keyboard import ReplyKeyboard, InlineKeyboard
+from utils.logger import log
