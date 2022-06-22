0c2a513ce87e071385e85d65cd743c479289f077
Alexander <nidillin@gmail.com>
2022-06-22 22:54:42 +0300

    changes relative to config file

--- bot/utils/logger.py
+++ bot/utils/logger.py
@@ -1,9 +1,10 @@
 from cgitb import handler
+import config
 import logging
 
 log = logging.getLogger("bot.py")
 log.setLevel(logging.INFO)
-fileHandler = logging.FileHandler("../Logs/bot.log")
+fileHandler = logging.FileHandler(config.log_path)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 fileHandler.setFormatter(formatter)
 log.addHandler(fileHandler)
