ece5f670605195f26bb17151ee6942ce8d4b40ac
Alexander <nidillin@gmail.com>
2022-06-26 16:53:27 +0300

    import DB

--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -1,4 +1,4 @@
-from utils.db import UserDB, BoarDB, JokeDB, MsgDB, PicDB, Statistics, new_suggestions
+from utils.db import UserDB, BoarDB, JokeDB, MsgDB, PicDB, Statistics, new_suggestions, DB
 from utils.menu import Menu
 from utils.upload import UploadPic
 from utils.keyboard import ReplyKeyboard, InlineKeyboard
