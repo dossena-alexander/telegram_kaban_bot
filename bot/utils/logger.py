86c3fb42dff98f2d29dc6ed470b305a6375de25a
Alexander <nidillin@gmail.com>
2022-07-02 23:33:56 +0300

    Update path to "Log" folder

--- bot/utils/logger.py
+++ bot/utils/logger.py
@@ -1,10 +1,10 @@
 from cgitb import handler
-import config
+from config import PATH
 import logging
 
 log = logging.getLogger("bot.py")
 log.setLevel(logging.INFO)
-fileHandler = logging.FileHandler(config.log_path)
+fileHandler = logging.FileHandler(PATH.LOG)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 fileHandler.setFormatter(formatter)
 log.addHandler(fileHandler)
