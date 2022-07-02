4c4c1f81879f4200fa6582da7cb631bdac384c7c
Alexander <nidillin@gmail.com>
2022-07-02 23:35:24 +0300

    update shedule func

--- bot/handlers/txt_worker.py
+++ bot/handlers/txt_worker.py
@@ -1,12 +1,13 @@
-from header import adminPicDB, random, adminJokeDB, bot, shedule, utils, config
-from user.user_funcs import getWct
+from header import adminPicDB, random, adminJokeDB, bot, shedule, utils
+from config import PATH, SHEDULE_SITE
+from user import getWct
 
 
 def textWorker(message):
     msg = message.text.lower()
     if msg == "фотокарточка":
         bot.send_photo(message.chat.id, 
-        open(config.photos_path + adminPicDB.getPicID(recNum=random.randint(0, adminPicDB.getRecCount() - 1)), "rb"))
+        open(PATH.PHOTOS + adminPicDB.getRecord(recNum=random.randint(0, adminPicDB.getRecCount() - 1)), "rb"))
     elif msg == "анекдот":
         bot.send_message(message.chat.id, 
         adminJokeDB.getRecord(recNum=random.randint(0, adminJokeDB.getRecCount() - 1)))
@@ -15,5 +16,5 @@ def textWorker(message):
             bot.send_photo(message.chat.id, getWct(message))
     elif msg == "расписание":
         key = utils.InlineKeyboard()
-        key.addUrlButton("Сайт с заменами", "http://www.lmk-lipetsk.ru/main_razdel/shedule/index.php")
-        bot.send_photo(message.chat.id, open(shedule.getShedule(), 'rb'), shedule.currentDay(), reply_markup=key.get())
+        key.addUrlButton("Сайт с заменами", SHEDULE_SITE)
+        bot.send_photo(message.chat.id, shedule.getShedule(), shedule.currentDay(), reply_markup=key.get())
