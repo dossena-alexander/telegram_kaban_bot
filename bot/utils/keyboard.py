f4726afbbd4e7be5ba0a722d710ebfb07002f79a
Alexander <nidillin@gmail.com>
2022-07-02 23:39:46 +0300

    Big changes. Keyboard now gets keys as dictionary. So, "add" and "autoRow" was deleted. Added method "add_button"

--- bot/utils/keyboard.py
+++ bot/utils/keyboard.py
@@ -10,29 +10,30 @@ class ReplyKeyboard():
         return self.keyboard
 
 
-    def add(self, keys: list) -> None:
-        self._keys = keys
-        for button in keys:
-            self.keyboard.add(button)
+    def set_keyboard(self, keys: dict) -> None:
+        """
+        Keys as dictionary is rowing in two buttons grid.
+        The parametred dict will used to set it up to tuples list for further rowing.
+        """
+        buttons_as_listed_tuples = []
+
+        for item in keys.values():
+            buttons_as_listed_tuples.append(item["text"])
+
+        lt = buttons_as_listed_tuples
+        for j in range(len(lt)//2 + len(lt)%2):
+            i = j*2
+            result = [item for item in lt[i:i+2]]
+            a = result[0]
+            if len(result) == 2: 
+                b = result[1]
+                self.keyboard.row(a, b)
+            else:
+                self.keyboard.row(a)
 
 
-    def autoRow(self) -> None:
-        """
-        one row -- two buttons
-        """
-        if len(self._keys) != 0:
-            del self.keyboard
-            self.keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            lt = self._keys
-            for j in range(len(lt)//2 + len(lt)%2):
-                i = j*2
-                result = [item for item in lt[i:i+2]]
-                a = result[0]
-                if len(result) == 2: 
-                    b = result[1]
-                    self.keyboard.row(a, b)
-                else:
-                    self.keyboard.row(a)
+    def add_button(self, text) -> None:
+        self.keyboard.add(text)
 
 
 class InlineKeyboard(): 
@@ -44,40 +45,43 @@ class InlineKeyboard():
         return self.keyboard
 
 
-    def add(self, keys: list) -> None:
+    def set_keyboard(self, keys: dict) -> None:
         """
-        keys text is button and callback data
+        Keys as dictionary is rowing in two buttons grid.
+        The parametred dict will used to set it up to tuples list for further rowing.
         """
-        self._keys = keys
-        for text in keys:
-            button = types.InlineKeyboardButton(text=text, callback_data=text)
-            self.keyboard.add(button)
+        buttons_as_listed_tuples = []
+
+        for item in keys.values():
+            buttons_as_listed_tuples.append((item["text"], item["call"]))
+
+        lt = buttons_as_listed_tuples
+        for j in range(len(lt)//2 + len(lt)%2):
+            i = j*2
+            result = [item for item in lt[i:i+2]]
+            a = types.InlineKeyboardButton(text=result[0][0], callback_data=result[0][1])
+            if len(result) == 2: 
+                b = types.InlineKeyboardButton(text=result[1][0], callback_data=result[1][1])
+                self.keyboard.row(a, b)
+            else:
+                self.keyboard.row(a)
 
 
-    def addUrlButton(self, key: str, url: str) -> None:
+    def add_url_button(self, key: str, url: str) -> None:
         button = types.InlineKeyboardButton(text=key, url=url)
         self.keyboard.add(button)
 
     
-    def rowUrlButton(self, key: str, url: str) -> None:
+    def row_url_button(self, key: str, url: str) -> None:
         button = types.InlineKeyboardButton(text=key, url=url)
         self.keyboard.row(button)
 
 
-    def autoRow(self) -> None:
-        """
-        one row -- two buttons
-        """
-        if len(self._keys) != 0:
-            del self.keyboard
-            self.keyboard = types.InlineKeyboardMarkup()
-            lt = self._keys
-            for j in range(len(lt)//2 + len(lt)%2):
-                i = j*2
-                result = [item for item in lt[i:i+2]]
-                a = types.InlineKeyboardButton(text=result[0], callback_data=result[0])
-                if len(result) == 2: 
-                    b = types.InlineKeyboardButton(text=result[1], callback_data=result[1])
-                    self.keyboard.row(a, b)
-                else:
-                    self.keyboard.row(a)
+    def add_button(self, text: str, call: str) -> None:
+        button = types.InlineKeyboardButton(text=text, callback_data=call)
+        self.keyboard.add(button)
+
+
+    # def row_button(self, text: str, call: str) -> None:
+    #     button = types.InlineKeyboardButton(text=text, callback_data=call)
+    #     self.keyboard.row(button)
