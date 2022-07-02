09825cb8fed3609fbe90769ae0d9d45bb86e873d
Alexander <nidillin@gmail.com>
2022-07-02 23:31:35 +0300

    new config using classes to strore data. Keys now is dictionary

--- bot/config.py
+++ bot/config.py
@@ -1,56 +1,103 @@
-token = ""
-adminID = 0
-#========================MSG===================================
+API_TOKEN = ""
+ADMIN_ID = 0
+#========================MSG======================================
 
-userMsg = "Меню пользователя"
-adminMsg = "Админ меню"
-helpMsg = "<b>Что я умею</b>\n <i>Мои команды:</i>\n • /start - Запуск\n • /help - Помощь\n • /auth - Аутентификация\n <i>Мои возможности:</i>\n • <b>Какой ты кабан сегодня</b>\n • <b>Фотокарточка</b> –– Смешная картинка\n • <b>Анекдот</b> –– Рандомный анекдот из тысячи"
-userSubMenuMsg = "Кабаний перевод телеграмм для настоящих кабанов, просто нажми на кнопку, чтобы установить"
+class BOT_MESSAGE():
+    USER = "Меню пользователя"
+    ADMIN = "Админ меню"
+    USER_SUB_MENU = "Кабаний перевод телеграмм для настоящих кабанов, просто нажми на кнопку, чтобы установить"
+    HELP = "<b>Что я умею</b>\n <i>Мои команды:</i>\n • /start - Запуск\n • /help - Помощь\n • /auth - Аутентификация\n <i>Мои возможности:</i>\n • <b>Какой ты кабан сегодня</b>\n • <b>Фотокарточка</b> –– Смешная картинка\n • <b>Анекдот</b> –– Рандомный анекдот из тысячи"
 
-#========================KEYS===================================
+#========================KEYS=====================================
 
-# keys below will be rowed as you see
-# In other words, in one row -- two buttons
-# keys is a button text and callback data in the same time
-
-adminKeys = [
-        "Предложения",         "Загрузка",
-         "Сообщения",          "Рассылка", 
-      "Остановить бота",     "Пользователь",
-                    "Статистика"
-]
-
-userKeys = [
-        "Загрузить", "Сообщение админу",
-          "Кабаний перевод телеграмм"
-]
-
-startKeys = [
-    "Анекдот", "Фотокарточка",
-    "Какой я кабан сегодня", "Расписание"
-]
+class KEYS():
+    ADMIN = {
+        0: { "text": "Предложения",               "call": "SUGGESTIONS"       }, 
+        1: { "text": "Загрузка",                  "call": "UPLOAD_MENU_ADMIN" },
+        2: { "text": "Сообщения",                 "call": "SEE_MESSAGES"      }, 
+        3: { "text": "Рассылка",                  "call": "NOTIFY"            },
+        4: { "text": "Остановить бота",           "call": "STOP_BOT"          },
+        5: { "text": "Пользователь",              "call": "USER"              }, 
+        6: { "text": "Статистика",                "call": "STATISTICS"        },
+    }
+    USER = {
+        0: { "text": "Загрузить",                 "call": "UPLOAD_MENU_USER"  }, 
+        1: { "text": "Сообщение админу",          "call": "MESSAGE_TO_ADMIN"  },
+        2: { "text": "Кабаний перевод телеграмм", "call": "TRANSLATE"         }, 
+    }
+    START = {
+        0: { "text": "Анекдот",                   "call": "None"              }, 
+        1: { "text": "Фотокарточка",              "call": "None"              },
+        2: { "text": "Какой я кабан сегодня",     "call": "None"              }, 
+    }
+    USER_SUB_MENU = {
+        0: { "text": "Русский",                   "call": "None"              }, 
+        1: { "text": "Кабаний",                   "call": "None"              },
+        2: { "text": "Назад",                     "call": "BACK_USER"         }, 
+    }
+    UPLOAD_MENU_USER = {
+        0: { "text": "Картинку",                  "call": "UPLOAD_PICTURE"    }, 
+        1: { "text": "Анекдот",                   "call": "UPLOAD_JOKE"       },
+        2: { "text": "Назад",                     "call": "BACK_USER"         }, 
+    }
+    UPLOAD_MENU_ADMIN = {
+        0: { "text": "Картинку",                  "call": "UPLOAD_PICTURE"    }, 
+        1: { "text": "Анекдот",                   "call": "UPLOAD_JOKE"       },
+        2: { "text": "Кабана",                    "call": "UPLOAD_BOAR"       }, 
+    }
+    JOKE_SEE = {
+        0: { "text": "Выйти",                     "call": "BACK_ADMIN"        }, 
+        1: { "text": "Далее",                     "call": "JOKE_FURTHER"      },
+        2: { "text": "Принять",                   "call": "JOKE_ACCEPT"       }, 
+        3: { "text": "Удалить",                   "call": "JOKE_DELETE"       }, 
+    }
+    PIC_SEE = {
+        0: { "text": "Выйти",                     "call": "BACK_ADMIN"        }, 
+        1: { "text": "Далее",                     "call": "PIC_FURTHER"       },
+        2: { "text": "Принять",                   "call": "PIC_ACCEPT"        }, 
+        3: { "text": "Удалить",                   "call": "PIC_DELETE"        }, 
+    }
+    MSG_SEE = {
+        0: { "text": "Выйти",                     "call": "BACK_ADMIN"        }, 
+        1: { "text": "Далее",                     "call": "MSG_FURTHER"       },
+        2: { "text": "Удалить",                   "call": "MSG_DELETE"        }, 
+    }
+    SUGGESTIONS = {
+        0: { "text": "Картинки",                  "call": "SEE_PICTURES"      }, 
+        1: { "text": "Анекдоты",                  "call": "SEE_JOKES"         },
+        2: { "text": "Назад",                     "call": "BACK_ADMIN"        }, 
+    }
 
 #========================LIMITS===================================
 
-#limits for user per day
-max_jokes_per_day = 10
-max_msgs_per_day = 5
-max_photos_per_day = 10
+# limits for user per day
+class LIMIT():
+    JOKE = 10
+    MESSAGE = 5
+    PHOTO = 10
 
 #========================FILTER===================================
 
-#text-worker filter
-word_filter = ["Фотокарточка", "Анекдот", "Какой я кабан сегодня", "Расписание"
-               "/auth", "/help", "/start"]
+# text-worker filter
+COMMANDS_FILTER = [
+            "Фотокарточка", 
+            "Анекдот", 
+            "Какой я кабан сегодня", 
+            "Расписание"
+            "/auth", "/help", "/start"
+]
 
-#========================PATHS===================================
+#========================PATHS====================================
 
-#folders
-wct_path = '../wct/'
-photos_path = '../photos/'
-recieved_photos_path = '../recieved_photos/'
-db_path = '../db/main.db'
-log_path = "../Logs/bot.log"
-shedule_path = "../Shedule/"
+# path to folders
+class PATH():
+    WCT = '../wct/'
+    PHOTOS = '../photos/'
+    RECIEVED_PHOTOS = '../recieved_photos/'
+    DB = '../db/main.db'
+    LOG = "../Logs/bot.log"
+    SHEDULE = "../Shedule/"
 
-#===========================================================
+#=================================================================
+
+SHEDULE_SITE = ""
