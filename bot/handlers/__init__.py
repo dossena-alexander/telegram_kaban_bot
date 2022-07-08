029f104f4b10bfbd4c978ad25d6535093fcea460
Alexander <nidillin@gmail.com>
2022-07-08 16:23:34 +0300

    import photoWorker

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,3 +1,4 @@
 from handlers.call_worker import callWorker
 from handlers.commands import start, auth, help, user, admin
 from handlers.txt_worker import textWorker
+from handlers.photo_worker import photoWorker
