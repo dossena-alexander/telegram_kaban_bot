2bb0d97f5f739063c252440797261490a306dee6
Alexander <nidillin@gmail.com>
2022-07-23 19:22:22 +0300

    photo handler

--- bot/bot.py
+++ bot/bot.py
@@ -1,4 +1,4 @@
-# v1.8.7
+# v1.8.8
 from handlers import *
 from header import *
 
@@ -6,13 +6,13 @@ from header import *
 bot.register_message_handler(commands=["start"], func=start, callback="none")
 bot.register_message_handler(commands=["auth"], func=auth, callback="none")
 bot.register_message_handler(commands=["help"], func=help, callback="none")
-bot.register_message_handler(content_types="text", func=textWorker, callback="none")
-bot.register_message_handler(content_types="photo", func=photoWorker, callback="none")
+bot.register_message_handler(content_types="text", func=reply_keyboard_worker, callback="none")
+bot.register_message_handler(content_types="photo", func=upload_photo_in_private_chat, callback="none")
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def call(call):
-    callWorker(call)
+    call_work(call)
 
 
 if __name__ == "__main__":
@@ -23,8 +23,3 @@ if __name__ == "__main__":
     except Exception as e:
         utils.log.error(e)
         bot.send_message(ADMIN_ID, "Bot stoped. Trouble occurred")
-        # you may use bot.polling() or sh script to restart bot.py
-        # for sh script use:
-        # import subprocess
-        # proc = subprocess.Popen('./your_sh.sh', stdout=subprocess.PIPE)
-        # output = proc.stdout.read()
