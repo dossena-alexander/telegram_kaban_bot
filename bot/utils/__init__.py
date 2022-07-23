a55aef7f4f3615fd5abdd8b30285f1dbba4ee212
Alexander <nidillin@gmail.com>
2022-07-23 19:13:25 +0300

    delete shedule

--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -1,6 +1,5 @@
-from utils.db import UserDB, BoarDB, PremiumBoarDB, JokeDB, MsgDB, PicDB, Statistics, suggestions, DB
+from utils.db import UserDB, BoarDB, PremiumBoarDB, JokeDB, MsgDB, PicDB, Statistics, suggestions, DB, Achievements, PremiumMenu, BoarsCategories
 from utils.menu import Menu
 from utils.upload import UploadPic
 from utils.keyboard import ReplyKeyboard, InlineKeyboard
 from utils.logger import log
-from utils.shedule import Shedule
