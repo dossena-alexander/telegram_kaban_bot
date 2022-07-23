0f2a5e2f135f7f6ac06c709cf23c11a608ee919c
Alexander <nidillin@gmail.com>
2022-07-23 19:09:33 +0300

    snake case. New private method "_row_buttons"

--- bot/utils/keyboard.py
+++ bot/utils/keyboard.py
@@ -3,11 +3,11 @@ from telebot import types
 
 class ReplyKeyboard():
     def __init__(self) -> None:
-        self.keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        self._keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
 
 
     def get(self) -> types.ReplyKeyboardMarkup:
-        return self.keyboard
+        return self._keyboard
 
 
     def set_keyboard(self, keys: dict) -> None:
@@ -20,29 +20,34 @@ class ReplyKeyboard():
         for item in keys.values():
             buttons_as_listed_tuples.append(item["text"])
 
-        lt = buttons_as_listed_tuples
-        for j in range(len(lt)//2 + len(lt)%2):
+        self._row_buttons(buttons_as_listed_tuples)
+
+
+    def _row_buttons(self, buttons):
+        for j in range(len(buttons)//2 + len(buttons)%2):
             i = j*2
-            result = [item for item in lt[i:i+2]]
+            result = [item for item in buttons[i:i+2]]
             a = result[0]
             if len(result) == 2: 
                 b = result[1]
-                self.keyboard.row(a, b)
+                self._keyboard.row(a, b)
             else:
-                self.keyboard.row(a)
+                self._keyboard.row(a)
 
 
     def add_button(self, text) -> None:
-        self.keyboard.add(text)
+        self._keyboard.add(text)
+
+
 
 
 class InlineKeyboard(): 
     def __init__(self) -> None:
-        self.keyboard = types.InlineKeyboardMarkup()
+        self._keyboard = types.InlineKeyboardMarkup()
 
 
     def get(self) -> types.InlineKeyboardMarkup:
-        return self.keyboard
+        return self._keyboard
 
 
     def set_keyboard(self, keys: dict) -> None:
@@ -54,27 +59,30 @@ class InlineKeyboard():
 
         for item in keys.values():
             buttons_as_listed_tuples.append((item["text"], item["call"]))
+            
+        self._row_buttons(buttons_as_listed_tuples)
 
-        lt = buttons_as_listed_tuples
-        for j in range(len(lt)//2 + len(lt)%2):
+    
+    def _row_buttons(self, buttons):
+        for j in range(len(buttons)//2 + len(buttons)%2):
             i = j*2
-            result = [item for item in lt[i:i+2]]
+            result = [item for item in buttons[i:i+2]]
             a = types.InlineKeyboardButton(text=result[0][0], callback_data=result[0][1])
             if len(result) == 2: 
                 b = types.InlineKeyboardButton(text=result[1][0], callback_data=result[1][1])
-                self.keyboard.row(a, b)
+                self._keyboard.row(a, b)
             else:
-                self.keyboard.row(a)
+                self._keyboard.row(a)
 
 
     def add_url_button(self, key: str, url: str) -> None:
         button = types.InlineKeyboardButton(text=key, url=url)
-        self.keyboard.add(button)
+        self._keyboard.add(button)
 
 
     def add_button(self, text: str, call: str) -> None:
         button = types.InlineKeyboardButton(text=text, callback_data=call)
-        self.keyboard.add(button)
+        self._keyboard.add(button)
 
 
     # def row_button(self, text: str, call: str) -> None:
