f13408c5549f597d36b9bff2e102fb49811b1de0
Alexander <nidillin@gmail.com>
2022-07-23 20:50:30 +0300

    ability to pass banned users if they send message to bot

---
+++ bot/utils/banned_users/ban_bot.py
@@ -0,0 +1,27 @@
+from telebot import TeleBot
+from telebot import apihelper
+from telebot import types
+
+from banned_users.banned import BannedDB
+
+
+class Ban_telebot(TeleBot):
+    def get_updates(self, *args, **kwargs):
+        json_updates = apihelper.get_updates(self.token, *args, **kwargs)
+        ret = []
+        for update in json_updates:
+            if update['message']['from']['id'] in self._banned_users():
+                self.last_update_id = update['update_id']
+            else:
+                ret.append(types.Update.de_json(update))
+
+        return ret
+
+    
+    def _banned_users(self) -> list[int]:
+        banned = BannedDB()
+        banned_users = banned.get_users()
+        if not banned_users:
+            banned_users = [0]
+
+        return banned_users
