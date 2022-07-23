eeedb22a5121656532e13085f1ec88e6c5b71140
Alexander <nidillin@gmail.com>
2022-07-23 19:22:10 +0300

    little changes

--- bot/header.py
+++ bot/header.py
@@ -13,20 +13,20 @@ bot = telebot.TeleBot(token=API_TOKEN)
 userDB = utils.UserDB()
 boarDB = utils.BoarDB()
 premiumBoarDB = utils.PremiumBoarDB()
+boarsCategories = utils.BoarsCategories()
 msgDB = utils.MsgDB()
 
-adminJokeDB = utils.JokeDB("adminJokes")
-adminPicDB = utils.PicDB("accPics")
+adminJokeDB = utils.JokeDB(table = "adminJokes")
+adminPicDB = utils.PicDB(table = "accPics")
 
-userPicDB = utils.PicDB("pics")
-userJokeDB = utils.JokeDB("userJokes")
+userPicDB = utils.PicDB(table = "pics")
+userJokeDB = utils.JokeDB(table = "userJokes")
 
 # records counter that`s used to see DB records in admin menu
 class mesg():
     count = 0
 
 suggestions = utils.suggestions()
-shedule = utils.Shedule()
 
 #========================MENU===================================
 
@@ -37,12 +37,13 @@ userSubMenu = utils.Menu()
 
 #========================MENU_SET-UP===================================
 
-adminMenu.setMsg(BOT_MESSAGE.ADMIN)
-adminMenu.setInlineKeyboard(KEYS.ADMIN)
+adminMenu.set_message(BOT_MESSAGE.ADMIN)
+adminMenu.set_inline_keyboard(KEYS.ADMIN)
 
-userMenu.setMsg(BOT_MESSAGE.USER)
-userMenu.setInlineKeyboard(KEYS.USER)
+userMenu.set_message(BOT_MESSAGE.USER)
+userMenu.set_inline_keyboard(KEYS.USER)
 
-userSubMenu.setMsg(BOT_MESSAGE.USER_SUB_MENU)
+userSubMenu.set_message(BOT_MESSAGE.USER_SUB_MENU)
 
-helpMenu.setMsg(BOT_MESSAGE.HELP(photo_count = adminPicDB.getRecCount(), joke_count = adminJokeDB.getRecCount()))
+helpMenu.set_message( BOT_MESSAGE.HELP(photo_count = adminPicDB.get_records_count(), 
+                                       joke_count = adminJokeDB.get_records_count()) )
