6590d34d8c85c3912b099d4f7982d08c7ed02265
Alexander <nidillin@gmail.com>
2022-07-23 20:50:14 +0300

    sorted __init__

--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -1,5 +1,8 @@
-from utils.db import UserDB, BoarDB, PremiumBoarDB, JokeDB, MsgDB, PicDB, Statistics, suggestions, DB, Achievements, PremiumMenu, BoarsCategories
+from utils.db import DB, UserDB, BoarDB, PremiumBoarDB, JokeDB, MsgDB, PicDB
+from utils.db import Statistics, suggestions
+from utils.db import Achievements, PremiumMenu, BoarsCategories
 from utils.menu import Menu
 from utils.upload import UploadPic
 from utils.keyboard import ReplyKeyboard, InlineKeyboard
 from utils.logger import log
+from utils.banned_users.ban_bot import Ban_telebot
