681e352b5de5b73ec5c970191cadd00ed1031a35
Alexander <nidillin@gmail.com>
2022-07-30 16:28:12 +0300

    upload to server at src with file_name

--- bot/utils/upload.py
+++ bot/utils/upload.py
@@ -9,12 +9,12 @@ class UploadPic():
         self.__src = src
 
 
-    def upload(self, file, file_info) -> None:
+    def upload(self, file, file_name) -> None:
         """
         Upload only photo in cause of telegram saving new photos to default folder "photos/"
         """
         log.info("UploadPic -- Загрузка файла на сервер")
-        source_file_name = self.__src + file_info.file_path.replace('photos/', '')
+        source_file_name = self.__src + file_name
         with open(source_file_name, 'wb') as new_file:
             new_file.write(file)
         log.info("Успешно")
