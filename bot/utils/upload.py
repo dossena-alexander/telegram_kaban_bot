8ae1c6766a7693fde493f2768c7dcf543336806d
Alexander <nidillin@gmail.com>
2022-08-17 13:25:26 +0300

    upload_lock

--- bot/utils/upload.py
+++ bot/utils/upload.py
@@ -1,4 +1,5 @@
 from utils.logger import log
+from config import UPLOAD_LOCK
 
 
 class UploadPic():
@@ -13,10 +14,11 @@ class UploadPic():
         """
         Upload only photo in cause of telegram saving new photos to default folder "photos/"
         """
-        log.info("UploadPic -- Загрузка файла на сервер")
-        source_file_name = self.__src + file_name
-        with open(source_file_name, 'wb') as new_file:
-            new_file.write(file)
-        log.info("Успешно")
+        if not UPLOAD_LOCK:
+            log.info("UploadPic -- Загрузка файла на сервер")
+            source_file_name = self.__src + file_name
+            with open(source_file_name, 'wb') as new_file:
+                new_file.write(file)
+            log.info("Успешно")
 
 
