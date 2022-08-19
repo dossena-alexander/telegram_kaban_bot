254794ccb206cfe8b75333e05ae38ea735a4d39e
Alexander <nidillin@gmail.com>
2022-08-19 17:29:40 +0300

    addons

---
+++ bot/server/addons/games/cross_zero/cross_zero.py
@@ -0,0 +1,23 @@
+from csv import field_size_limit
+
+
+class game():
+    _field_size: tuple
+
+
+    def __init__(self, field_size: tuple) -> None:
+        self._field_size = field_size
+
+    def set_zero(x: int, y: int):
+        pass
+
+    def set_cross(x: int, y: int):
+        pass
+
+
+cross_zero = game((3, 3))
+
+# /cross_zero id 3, 3
+
+# game = game(field).new_game()
+# game.set_zero(2, 1)
