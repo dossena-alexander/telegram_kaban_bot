24e93b421b0fead3020ece5b75f47508f8cdd81c
Alexander <nidillin@gmail.com>
2022-09-08 18:15:44 +0300

    new method send_notification()

--- bot/server/utils/ban/ban_bot.py
+++ bot/server/utils/ban/ban_bot.py
@@ -1,7 +1,10 @@
+from typing import Union, Optional
 from telebot import TeleBot
 from telebot import apihelper
 from telebot import types
+from telebot import REPLY_MARKUP_TYPES
 
+from header import userDB
 from server.utils.ban.banned import BannedDB
 
 
@@ -27,6 +30,30 @@ class Ban_telebot(TeleBot):
             else:
                 ret.append(types.Update.de_json(update))     
         return ret
+
+    def send_notification(
+            self, chat_id: Union[int, str], text: str, 
+            parse_mode: Optional[str]=None, 
+            entities: Optional[list[types.MessageEntity]]=None,
+            disable_web_page_preview: Optional[bool]=None, 
+            disable_notification: Optional[bool]=None, 
+            protect_content: Optional[bool]=None,
+            reply_to_message_id: Optional[int]=None, 
+            allow_sending_without_reply: Optional[bool]=None,
+            reply_markup: Optional[REPLY_MARKUP_TYPES]=None,
+            timeout: Optional[int]=None) -> types.Message:
+        if userDB.can_send_notification(chat_id):
+            self.send_message(
+                chat_id, text, 
+                parse_mode, 
+                entities,
+                disable_web_page_preview, 
+                disable_notification, 
+                protect_content,
+                reply_to_message_id, 
+                allow_sending_without_reply,
+                reply_markup,
+                timeout)
         
 
     
