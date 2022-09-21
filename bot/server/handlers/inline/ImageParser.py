a86ed42f8475d326acda87be7489bc08f0f99571
Alexander <nidillin@gmail.com>
2022-09-21 16:21:26 +0300

    Little beautiful

--- bot/server/handlers/inline/ImageParser.py
+++ bot/server/handlers/inline/ImageParser.py
@@ -1,17 +1,18 @@
+from dataclasses import dataclass
 import requests
 import json
 from fake_headers import Headers
 from bs4 import BeautifulSoup as bs4
 
 
-class Size:
-    def __init__(self):
-        self.large = 'large'
-        self.medium = 'medium'
-        self.small = 'small'
+@dataclass
+class _Size():
+    large: str = 'large'
+    medium: str = 'medium'
+    small: str = 'small'
 
 
-class Preview:
+class _Preview:
     def __init__(self, url: str,
                  width: int,
                  height: int):
@@ -21,14 +22,14 @@ class Preview:
         self.size = str(width) + '*' + str(height)
 
 
-class Result:
+class _Result:
     def __init__(self, title: str | None,
                  description: str | None,
                  domain: str,
                  url: str,
                  width: int,
                  height: int,
-                 preview: Preview):
+                 preview: _Preview):
         self.title = title
         self.description = description
         self.domain = domain
@@ -41,23 +42,20 @@ class Result:
 
 class YandexImage:
     def __init__(self):
-        self.size = Size()
         self.headers = Headers(headers=True).generate()
-        self.version = '1.0-release'
-        self.about = 'Yandex Images Parser'
 
-    def search(self, query: str, sizes: Size = 'large') -> list:
+    def search(self, query: str, size: str = _Size.large) -> list[_Result]:
         request = requests.get('https://yandex.ru/images/search',
                                params={"text": query,
                                        "nomisspell": 1,
                                        "noreask": 1,
-                                       "isize": sizes
+                                       "isize": size
                                        },
                                headers=self.headers)
 
         soup = bs4(request.text, 'html.parser')
         items_place = soup.find('div', {"class": "serp-list"})
-        output = list()
+        output = []
         try:
             items = items_place.find_all("div", {"class": "serp-item"})
         except AttributeError:
@@ -84,13 +82,13 @@ class YandexImage:
             preview_width = data['serp-item']['thumb']['size']['width']
             preview_height = data['serp-item']['thumb']['size']['height']
 
-            output.append(Result(title, description, domain, image,
+            output.append(_Result(title, description, domain, image,
                                  image_width, image_height,
-                                 Preview(preview, preview_width, preview_height)))
+                                 _Preview(preview, preview_width, preview_height)))
 
         return output
 
-    def gen_search(self, query: str, sizes: Size = 'large') -> Result:
+    def gen_search(self, query: str, sizes: _Size = 'large') -> _Result:
         request = requests.get('https://yandex.ru/images/search',
                                params={"text": query,
                                        "nomisspell": 1,
@@ -127,6 +125,6 @@ class YandexImage:
             preview_width = data['serp-item']['thumb']['size']['width']
             preview_height = data['serp-item']['thumb']['size']['height']
 
-            yield Result(title, description, domain, image,
+            yield _Result(title, description, domain, image,
                                  image_width, image_height,
-                                 Preview(preview, preview_width, preview_height))
+                                 _Preview(preview, preview_width, preview_height))
