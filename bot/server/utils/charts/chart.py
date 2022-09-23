3eddce752f3b1358c90eafbc2480f3aa8d286cab
NIDILLIN <writebutread@outlook.com>
2022-09-23 23:05:54 +0300

    X formatter

--- bot/server/utils/charts/chart.py
+++ bot/server/utils/charts/chart.py
@@ -9,14 +9,17 @@ from server.utils.charts.collector import IStatClickCollector, Time
 
 class Chart():
     """Draw chart by Collector data"""
+    date_format: str = '%Y-%M-%D'
+    time_format: str = '%H:%M'
+
     def __init__(self, path: str, 
                        collector: IStatClickCollector, 
-                       mode = 0, 
-                       dpi = 350) -> None:
+                       mode: str = 'time', 
+                       dpi: int = 350) -> None:
         """Args:
             path (str): path to save figure
             collector (IStatClickCollector): _description_
-            mode (int, optional): _description_. Defaults to 0.
+            mode (str, optional): X interval format 
             dpi (int, optional): Quality of figure. Defaults to 350.
             fig_name (str, optional): File name to save. Defaults to 'stat.jpg'.
         """
@@ -25,33 +28,41 @@ class Chart():
         self.mode = mode
         self.dpi = dpi
         self.fig_name = f'{collector.date} ({Time(now=True).time}).jpg'
-        self.fmt = dates.DateFormatter('%H:%M')
+        self.fmt = dates.DateFormatter(self.time_format)
         self.colors =  [ # inverted colors
-            '#00ffff', # red
-            '#ffff00', # blue
-            '#11bffe', # orange
-            '#6bff2b', # violet
-            '#ff00ff' # green
+            '#00ffff',   # red
+            '#ffff00',   # blue
+            '#11bffe',   # orange
+            '#6bff2b',   # violet
+            '#ff00ff'    # green
         ]
 
     def draw(self) -> None:
+        formatter = self.time_format
+        if self.mode == 'date':
+            formatter = self.date_format
+
         fig, ax = plt.subplots()
         plt.title('График использования', fontsize=20, fontname='Helvetica')
         plt.xlabel('Время', color='gray')
         plt.ylabel('Кол-во',color='gray')
+
         data_dict = self.get_data(self.collector)
         for c, target in enumerate(self.collector.targets):
             time_interval = data_dict[target]['times']
-            clicks = data_dict[target]['clicks']
-            if time_interval and clicks: # If lists have values
-                ax.plot(time_interval, clicks, "-o", color=self.colors[c], label=target)
+            if time_interval:
+                times = [datetime.strptime(time, formatter) for time in time_interval]
+                clicks = data_dict[target]['clicks']
+                ax.plot(times, clicks, "-o", color=self.colors[c], label=target)
         try:
             ax.legend()
         except:
             pass
-        # ax.xaxis.set_major_formatter(self.fmt)
+
+        ax.xaxis.set_major_formatter(self.fmt)
         fig.autofmt_xdate()
         plt.savefig(self.path+self.fig_name, dpi=self.dpi)
+
         image = Image.open(self.path+self.fig_name)
         inverted_image = PIL.ImageOps.invert(image)
         inverted_image.save(self.path+self.fig_name)
@@ -64,15 +75,3 @@ class Chart():
             tuple[list[datetime], int]: times_interval, clicks
         """
         return collector.get_data()
-
-    def set_data_type(self, data_type: str) -> None:
-        """For getting data from DataBase table must be named as data_type.\n
-        data_type could be:
-            'wct_clicks',
-            'photo_clicks',
-            'joke_clicks'
-
-        Args:
-            data_type (str): Table name
-        """
-        self.data_type = data_type
